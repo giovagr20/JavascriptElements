f8d0b338df50e39ba74a840dffa672d88c7d312a
Giovanni G R <giovannyg32@gmail.com>
2021-01-10 11:02:10 -0500

    POST Reqest AJAX

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,4 +1,6 @@
-let peticion;
+//get
+
+/* let peticion;
 
 if(window.XMLHttpRequest) peticion = new XMLHttpRequest();
 else peticion = new ActiveXObject("Microsoft.XMLHTTP");
@@ -12,4 +14,27 @@ peticion.addEventListener("load",()=>{
 
 peticion.open('GET','informacion.txt');
 
-peticion.send();
+peticion.send(); */
+
+let peticion;
+
+if(window.XMLHttpRequest) peticion = new XMLHttpRequest();
+else peticion = new ActiveXObject("Microsoft.XMLHTTP"); 
+
+const body = {
+    "name": "Gio",
+    "job": "Dev"
+}
+
+peticion.addEventListener('onload', ()=> {
+    let response;
+    if(peticion.status == 201 || peticion.status == 200)  response = peticion.response;
+    else response = "No ha cargado la info";
+
+    console.log(JSON.parse(response));
+});
+
+peticion.open('POST', 'https://reqres.in/api/users');
+
+peticion.setRequestHeader('Content.Type','application/json; charset=UTF8');
+peticion.send(JSON.stringify(body));
